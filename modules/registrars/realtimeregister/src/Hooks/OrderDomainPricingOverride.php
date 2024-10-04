ffa0ba383f5e5f9bec2d2876a2088d4b9df3b2af
Hylke Witjens <hylke.witjens@realtimeregister.com>
2024-10-04 14:32:44 +0200

    RT-15339 WHMCS doesn't use the premium pricing by default in the admin...

---
+++ modules/registrars/realtimeregister/src/Hooks/OrderDomainPricingOverride.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RealtimeRegister\Hooks;
+
+use RealtimeRegister\App;
+use RealtimeRegister\Entities\DataObject;
+use RealtimeRegister\PunyCode;
+
+class OrderDomainPricingOverride extends Hook
+{
+    use PunyCode;
+
+    public function __invoke(DataObject $vars): ?int
+    {
+        if ($vars['type'] === 'register') {
+            $res = App::client()->domains->check($vars['domain']);
+
+            if ($res->price !== null) {
+                return $res->price;
+            }
+        }
+    }
+}

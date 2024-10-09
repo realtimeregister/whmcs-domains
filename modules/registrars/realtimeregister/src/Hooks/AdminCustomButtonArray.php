f0c555d2fca4627d809883dac38eb33de4edb8b4
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2024-10-09 16:15:45 +0200

    RT-15387: fix exception handling adminbuttoncustomarray

--- modules/registrars/realtimeregister/src/Hooks/AdminCustomButtonArray.php
+++ modules/registrars/realtimeregister/src/Hooks/AdminCustomButtonArray.php
@@ -6,10 +6,14 @@ use RealtimeRegister\Actions\Action;
 use RealtimeRegister\App;
 use RealtimeRegister\Models\Whmcs\Domain;
 use RealtimeRegister\Request;
+use RealtimeRegister\Services\LogService;
 use SandwaveIo\RealtimeRegister\Domain\Enum\ResumeTypeEnum;
 
 class AdminCustomButtonArray extends Action
 {
+    /**
+     * @throws \Exception
+     */
     public function __invoke(Request $request): array|string
     {
         $metadata = $this->metadata($request);
@@ -29,42 +33,47 @@ class AdminCustomButtonArray extends Action
             }
         }
 
-        try {
-            $domain = $request->domain->domainName();
-            if (!empty($metadata->transferFOA)) {
-                $processes = App::client()->processes->list(
-                    parameters: [
-                        'status' => 'SUSPENDED',
-                        'action:in' => 'incomingInternalTransfer,incomingTransfer',
-                        "identifier:eq" => $request->domain->domainName()
-                    ]
-                );
+        $domain = $request->domain->domainName();
+        if (!empty($metadata->transferFOA)) {
+            $processes = App::client()->processes->list(
+                parameters: [
+                    'status' => 'SUSPENDED',
+                    'action:in' => 'incomingInternalTransfer,incomingTransfer',
+                    "identifier:eq" => $request->domain->domainName()
+                ]
+            );
 
-                if (
-                    $processes->count() > 0
-                    && in_array(ResumeTypeEnum::TYPE_RESEND, $processes->entities[0]->resumeTypes ?? [])
-                ) {
-                    $adminButtons['Resend FOA'] = "ResendTransfer";
+            if (
+                $processes->count() > 0
+                && in_array(ResumeTypeEnum::TYPE_RESEND, $processes->entities[0]->resumeTypes ?? [])
+            ) {
+                $adminButtons['Resend FOA'] = "ResendTransfer";
+            }
+        }
+        if ($request->params['regtype'] !== 'Transfer' && !empty($metadata->validationCategory)) {
+            try {
+                $info = App::client()->domains->get($domain);
+                if (in_array('PENDING_VALIDATION', $info->status)) {
+                    $adminButtons['Resend validation mails'] = "ResendValidationMails";
+                }
+            } catch (\SandwaveIo\RealtimeRegister\Exceptions\NotFoundException $exception) {
+                // Don't care about this exception at this point in the code
+            } catch (\Exception $ex) {
+                if (!str_contains($ex->getMessage(), 'Entity not found')) {
+                    throw $ex;
                 }
             }
-            if ($request->params['regtype'] !== 'Transfer' && !empty($metadata->validationCategory)) {
-                try {
-                    $info = App::client()->domains->get($domain);
-                    if (in_array('PENDING_VALIDATION', $info->status)) {
-                        $adminButtons['Resend validation mails'] = "ResendValidationMails";
-                    }
-                } catch (\SandwaveIo\RealtimeRegister\Exceptions\NotFoundException $exception) {
-                    // Don't care about this exception at this point in the code
-                } catch (\Exception $ex) {
-                    if (!str_contains($ex->getMessage(), 'Entity not found')) {
-                        throw $ex;
-                    }
-                }
-            }
-        } catch (\Exception $ex) {
-            return sprintf('Error retrieving information about domain: %s.', $ex->getMessage());
         }
 
         return $adminButtons;
+    }
+
+    public static function handleException(\Throwable $exception, array $params) : array
+    {
+        LogService::logError($exception);
+        return [
+            "success" => false,
+            sprintf('Error retrieving information about domain: %s.', $exception->getMessage())
+        ];
     }
 }

751928d56250179f1da8dd29105a8d83ccd7f72f
Hylke Witjens <hylke.witjens@realtimeregister.com>
2024-10-23 13:25:19 +0200

    RT-15441 Make coopAcceptRequirements required

--- modules/registrars/realtimeregister/additionalfields.php
+++ modules/registrars/realtimeregister/additionalfields.php
@@ -95,6 +95,12 @@ if (!empty($additionaldomainfields['.eu'])) {
             }
         }
     }
+} else if (!empty($additionaldomainfields['.coop'])) {
+    foreach ($additionaldomainfields['.coop'] as $i => $field) {
+        if ($field['Name'] === 'coopAcceptRequirements') {
+            $additionaldomainfields['.coop'][$i]['Required'] = true;
+        }
+    }
 }
 
 MetadataService::removeDefaultFields($additionaldomainfields);

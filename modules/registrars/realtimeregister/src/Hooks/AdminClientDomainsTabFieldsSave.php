e15492ae98fc4c2ab38ad01bd702edeeb6e84abb
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2024-10-24 13:09:27 +0200

    RT-15438: fixes for fields save

--- modules/registrars/realtimeregister/src/Hooks/AdminClientDomainsTabFieldsSave.php
+++ modules/registrars/realtimeregister/src/Hooks/AdminClientDomainsTabFieldsSave.php
@@ -32,9 +32,10 @@ class AdminClientDomainsTabFieldsSave extends Hook
                 return;
             }
 
-            $newProperties = array_combine(
-                array_column($metadataProperties, 'name'),
-                $vars['domainfield'] ?? []
+            $newProperties = array_filter(
+                array_combine(array_column(self::getFieldNames($vars['id']), 'name'), $vars['domainfield'] ?? []
+            ), fn($key) => $key !== 'languageCode',
+                ARRAY_FILTER_USE_KEY
             );
 
             $order = App::localApi()->order($domain['orderid']);
@@ -54,6 +55,15 @@ class AdminClientDomainsTabFieldsSave extends Hook
                 }
             }
         }
+    }
+
+    private static function getFieldNames(int $domainId): array
+    {
+        return AdditionalFields::query()
+            ->select(["name"])
+            ->where("domainid", '=', $domainId)
+            ->get()
+            ->toArray();
     }
 
     private static function revertChanges($handle, $tldInfo, $domainId): void

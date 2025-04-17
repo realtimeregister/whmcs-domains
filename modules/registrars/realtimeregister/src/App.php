53ee72bef225e2654c4473d037b2463ca9e1261c
Hylke Witjens <hylke.witjens@realtimeregister.com>
2025-04-17 09:21:06 +0200

    New version

--- modules/registrars/realtimeregister/src/App.php
+++ modules/registrars/realtimeregister/src/App.php
@@ -20,7 +20,7 @@ use TrueBV\Punycode;
 class App
 {
     public const NAME = 'realtimeregister';
-    public const VERSION = '2.3.3';
+    public const VERSION = '2.3.5';
 
     protected const API_URL = "https://api.yoursrs.com/";
     protected const API_URL_TEST = "https://api.yoursrs-ote.com/";

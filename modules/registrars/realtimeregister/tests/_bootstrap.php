cf07da05bf314d9a6039a02efd808f457a65526a
Hylke Witjens <hylke.witjens@realtimeregister.com>
2025-04-28 15:04:38 +0200

    Trying to fix github action

--- modules/registrars/realtimeregister/tests/_bootstrap.php
+++ modules/registrars/realtimeregister/tests/_bootstrap.php
@@ -18,6 +18,9 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule();
 
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
 $capsule->addConnection([
     'driver' => 'sqlite',
     'database' => ':memory:',
@@ -29,9 +32,6 @@ use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 
 $capsule->setEventDispatcher(new Dispatcher(new Container()));
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
 
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();

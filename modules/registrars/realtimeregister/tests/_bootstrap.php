76871b41149413f71d1182943e4ed66d5aa9f5b5
Hylke Witjens <hylke.witjens@realtimeregister.com>
2025-04-28 15:35:39 +0200

    Removed test

--- modules/registrars/realtimeregister/tests/_bootstrap.php
+++ modules/registrars/realtimeregister/tests/_bootstrap.php
@@ -14,50 +14,6 @@ if (!defined('ROOTDIR')) {
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-$capsule = new Capsule();
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-$capsule->addConnection([
-    'driver' => 'sqlite',
-    'database' => ':memory:',
-    'prefix' => '',
-]);
-
-// Set the event dispatcher used by Eloquent models... (optional)
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-
-$capsule->setEventDispatcher(new Dispatcher(new Container()));
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
-
-// Initialize database schema for testing
-// Create Cache table
-\RealtimeRegisterDomains\Models\RealtimeRegister\Cache::boot();
-
-// Create ContactMapping table if it doesn't exist
-if (!$capsule::schema()->hasTable(\RealtimeRegisterDomains\Models\RealtimeRegister\ContactMapping::TABLE_NAME)) {
-    $capsule::schema()->create(
-        \RealtimeRegisterDomains\Models\RealtimeRegister\ContactMapping::TABLE_NAME,
-        function ($table) {
-            $table->integer('userid');
-            $table->integer('contactid');
-            $table->char('handle', 40);
-            $table->boolean('org_allowed');
-            $table->unique(
-                ['userid', 'contactid', 'org_allowed'],
-                'mod_realtimeregister_contact_mapping_unique_contact'
-            );
-            $table->unique('handle', 'mod_realtimeregister_contact_mapping_unique_handle');
-        }
-    );
-}
-
 // Include the WHMCS module.
 require_once __DIR__ . '/../realtimeregister.php';
 

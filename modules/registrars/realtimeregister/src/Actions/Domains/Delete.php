5f8b4f5040fa531bbcc1bae52d78dd115d0bce66
Kevin <koster.k2001@gmail.com>
2025-12-04 13:46:09 +0100

    RT-17455: fix domain delete (#146)

--- modules/registrars/realtimeregister/src/Actions/Domains/Delete.php
+++ modules/registrars/realtimeregister/src/Actions/Domains/Delete.php
@@ -12,11 +12,12 @@ class Delete extends Action
 {
     public function __invoke(Request $request): array
     {
-        App::client()->domains->delete(self::getDomainName($request->domain));
+        $domainName = self::getDomainName($request->domain);
+        App::client()->domains->delete($domainName);
         $this->forgetDomainInfo($request);
 
         try {
-            Manager::table(InactiveDomains::TABLE_NAME)->where(['domainName' => $request->domain])
+            Manager::table(InactiveDomains::TABLE_NAME)->where(['domainName' => $domainName])
                 ->delete();
         } catch (\Exception $ignored) {
         }

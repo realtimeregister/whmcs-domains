0f1bdce983843aa6f0420fbe8a74d3d8e6d026d3
pieterjan.eilers <pieterjan.eilers@realtimeregister.com>
2026-01-19 09:58:32 +0100

    Update version, no dev-master in composer.lock

--- modules/registrars/realtimeregister/src/App.php
+++ modules/registrars/realtimeregister/src/App.php
@@ -21,7 +21,7 @@ use TrueBV\Punycode;
 class App
 {
     public const NAME = 'realtimeregister';
-    public const VERSION = '2.5.1';
+    public const VERSION = '2.5.2';
     private const CACHE_KEY_VERSION = 'realtimeregister_domains_version_number';
 
     protected const API_URL = "https://api.yoursrs.com/";

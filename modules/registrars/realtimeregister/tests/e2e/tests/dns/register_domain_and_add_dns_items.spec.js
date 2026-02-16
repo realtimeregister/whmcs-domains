b51845c047fc9fbb91afe5fc46c9e13932c9621e
Hylke Witjens <hylke.witjens@realtimeregister.com>
2026-02-16 14:18:40 +0100

    Enabled all tests

--- modules/registrars/realtimeregister/tests/e2e/tests/dns/register_domain_and_add_dns_items.spec.js
+++ modules/registrars/realtimeregister/tests/e2e/tests/dns/register_domain_and_add_dns_items.spec.js
@@ -48,69 +48,64 @@ test ('Set dns setup to BASIC and order domain', async ({ page }) => {
     await addDnsItem(page, domain)
 });
 
-// test ('Set dns setup to PREMIUM and order domain', async ({ page }) => {
-//     await loginAsAdmin(page)
-//
-//     // Set default nameservers
-//     await page.goto('/admin/configgeneral.php#tab=4');
-//     if (page.getByRole('textbox', {name: 'Password'})) {
-//         await page.getByRole('textbox', {name: 'Password'}).fill(process.env.ADMIN_PASSWORD)
-//         await page.getByRole('button', { name: 'Confirm Password' }).click()
-//     }
-//     await page.fill('input[name="ns1"]', process.env.PREMIUM_NAMESERVER_1)
-//     await page.fill('input[name="ns2"]', process.env.PREMIUM_NAMESERVER_2)
-//
-//     // change config of WHMCS
-//     await page.goto('/admin/configregistrars.php')
-//     await page.locator('td:nth-child(3) > .btn.btn-default').click();
-//     await page.selectOption('select[name="dns_support"]', 'PREMIUM')
-//     await page.fill('input[name="dns_vanity_nameserver_1"]', '')
-//     await page.fill('input[name="dns_vanity_nameserver_2"]', '')
-//     await page.getByRole('button', {name: 'save'}).click()
-//
-//     // logout
-//     await page.goto('/admin/logout.php')
-//
-//     await loginAsUser(page)
-//     let domain = generateDomainName()
-//
-//     await orderDomain(page, domain, true)
-//     await addDnsItem(page, domain)
-//
-//
-//     // domain = generateDomainName();
-//     // await orderDomain(page, domain, false)
-// });
+test ('Set dns setup to PREMIUM and order domain', async ({ page }) => {
+    await loginAsAdmin(page)
+
+    // Set default nameservers
+    await page.goto('/admin/configgeneral.php#tab=4');
+    if (page.getByRole('textbox', {name: 'Password'})) {
+        await page.getByRole('textbox', {name: 'Password'}).fill(process.env.ADMIN_PASSWORD)
+        await page.getByRole('button', { name: 'Confirm Password' }).click()
+    }
+    await page.fill('input[name="ns1"]', process.env.PREMIUM_NAMESERVER_1)
+    await page.fill('input[name="ns2"]', process.env.PREMIUM_NAMESERVER_2)
+
+    // change config of WHMCS
+    await page.goto('/admin/configregistrars.php')
+    await page.locator('td:nth-child(3) > .btn.btn-default').click();
+    await page.selectOption('select[name="dns_support"]', 'PREMIUM')
+    await page.fill('input[name="dns_vanity_nameserver_1"]', '')
+    await page.fill('input[name="dns_vanity_nameserver_2"]', '')
+    await page.getByRole('button', {name: 'save'}).click()
+
+    // logout
+    await page.goto('/admin/logout.php')
+
+    await loginAsUser(page)
+    let domain = generateDomainName()
+
+    await orderDomain(page, domain, true)
+    await addDnsItem(page, domain)
+});
 
 
-// test ('Set dns setup to PREMIUM with custom nameservers and order domain', async ({ page }) => {
-//     await loginAsAdmin(page)
-//
-//     // Set default nameservers
-//     await page.goto('/admin/configgeneral.php#tab=4');
-//     if (page.getByRole('textbox', {name: 'Password'})) {
-//         await page.getByRole('textbox', {name: 'Password'}).fill(process.env.ADMIN_PASSWORD)
-//         await page.getByRole('button', { name: 'Confirm Password' }).click()
-//     }
-//
-//     await page.fill('input[name="ns1"]', process.env.PREMIUM_NAMESERVER_1)
-//     await page.fill('input[name="ns2"]', process.env.PREMIUM_NAMESERVER_2)
-//
-//     // change config of WHMCS
-//     await page.goto('/admin/configregistrars.php')
-//     await page.locator('td:nth-child(3) > .btn.btn-default').click();
-//     await page.selectOption('select[name="dns_support"]', 'PREMIUM')
-//     await page.fill('input[name="dns_vanity_nameserver_1"]', process.env.VANITY_NAMESERVER_1)
-//     await page.fill('input[name="dns_vanity_nameserver_2"]', process.env.VANITY_NAMESERVER_2)
-//     await page.getByRole('button', {name: 'save'}).click()
-//
-//     // logout
-//     await page.goto('/admin/logout.php')
-//
-//     await loginAsUser(page)
-//
-//     let domain = generateDomainName()
-//     await orderDomain(page, domain, true)
-//     domain = generateDomainName()
-//     await orderDomain(page, domain, false)
-// });
+test ('Set dns setup to PREMIUM with custom nameservers and order domain', async ({ page }) => {
+    await loginAsAdmin(page)
+
+    // Set default nameservers
+    await page.goto('/admin/configgeneral.php#tab=4');
+    if (page.getByRole('textbox', {name: 'Password'})) {
+        await page.getByRole('textbox', {name: 'Password'}).fill(process.env.ADMIN_PASSWORD)
+        await page.getByRole('button', { name: 'Confirm Password' }).click()
+    }
+
+    await page.fill('input[name="ns1"]', process.env.PREMIUM_NAMESERVER_1)
+    await page.fill('input[name="ns2"]', process.env.PREMIUM_NAMESERVER_2)
+
+    // change config of WHMCS
+    await page.goto('/admin/configregistrars.php')
+    await page.locator('td:nth-child(3) > .btn.btn-default').click();
+    await page.selectOption('select[name="dns_support"]', 'PREMIUM')
+    await page.fill('input[name="dns_vanity_nameserver_1"]', process.env.VANITY_NAMESERVER_1)
+    await page.fill('input[name="dns_vanity_nameserver_2"]', process.env.VANITY_NAMESERVER_2)
+    await page.getByRole('button', {name: 'save'}).click()
+
+    // logout
+    await page.goto('/admin/logout.php')
+
+    await loginAsUser(page)
+
+    let domain = generateDomainName()
+    await orderDomain(page, domain, true)
+    await addDnsItem(page, domain)
+});
